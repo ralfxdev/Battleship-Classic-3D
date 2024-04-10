fa677886c200692661e0bcd00397d297585c7a12
ralfxdev <lopezalexander1019@gmail.com>
2024-04-10 02:33:29 -0600

    Added rotation and configured tag for planes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] ships;
+    [Header("Hud")]
+    public Button nextBtn;
+    public Button rotateBtn;
+
     private bool setupComplete = false;
     private bool playerTurn = true;
     private int shipIndex = 0;
@@ -13,6 +19,18 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         shipScript = ships[shipIndex].GetComponent<ShipScript>();
+        nextBtn.onClick.AddListener(() => NextShipClicked());
+        rotateBtn.onClick.AddListener(() => RotateClicked());
+    }
+
+    private void NextShipClicked()
+    {
+        if (shipIndex < ships.Length - 1)
+        {
+            shipIndex++;
+            shipScript = ships[shipIndex].GetComponent<ShipScript>();
+            //shipScript.FlashColor(Color.yellow);
+        }
     }
 
     void Update()
@@ -28,6 +46,7 @@ public class GameManager : MonoBehaviour
         }else if (!setupComplete)
         {
             PlaceShip(tile);
+            shipScript.SetClickedTile(tile);
         }
     }
 
@@ -37,5 +56,15 @@ public class GameManager : MonoBehaviour
         shipScript.ClearTileList();
         Vector3 newVec = shipScript.GetOffsetVect(tile.transform.position);
         ships[shipIndex].transform.localPosition = newVec;
+    }
+
+    private void RotateClicked()
+    {
+        shipScript.RotateShip();
+    }
+
+    private void SetShipClickedTile(GameObject tile)
+    {
+        shipScript.SetClickedTile(tile);
     }
 }
--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -7,6 +7,8 @@ public class ShipScript : MonoBehaviour
     List<GameObject> touchTiles = new List<GameObject>();
     public float xOffset = 0;
     public float zOffset = 0;
+    public float nextZRotation = 90f;
+    private GameObject clickedTile;
 
     void Start()
     {
@@ -26,5 +28,35 @@ public class ShipScript : MonoBehaviour
     public Vector3 GetOffsetVect(Vector3 tilePos)
     {
         return new Vector3(tilePos.x + xOffset, 2, tilePos.z + zOffset);
+    }
+
+    public void RotateShip()
+    {
+        touchTiles.Clear();
+        if (gameObject.CompareTag("Plane"))
+        {
+            // Rotate on the Y axis for airplanes
+            transform.localEulerAngles += new Vector3(0, nextZRotation, 0);
+        }
+        else
+        {
+            // Rotate on Z axis for boats
+            transform.localEulerAngles += new Vector3(0, 0, nextZRotation);
+        }
+        nextZRotation *= -1;
+        float temp = xOffset;
+        xOffset = zOffset;
+        zOffset = temp;
+        SetPosition(clickedTile.transform.position);
+    }
+
+    public void SetPosition(Vector3 newVect)
+    {
+        transform.localPosition = new Vector3(newVect.x + xOffset, 2, newVect.z +zOffset);
+    }
+
+    public void SetClickedTile(GameObject tile)
+    {
+        clickedTile = tile;
     }
 }
